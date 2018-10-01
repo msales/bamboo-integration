250e973e6c7e63fa648e3f70726106cd36594ef2
Bartlomiej (Bart) Rekke <bojleros@gmail.com>
2018-10-01 11:37:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'Flask Dockerized uh oh !'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=80)
